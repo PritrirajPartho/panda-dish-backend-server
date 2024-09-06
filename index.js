ba87950b86090acceddd4cab29a7c962d142da67
Pritiraj Partho <dpartho5616@gmail.com>
2024-09-06 09:33:10 +0600

    first commit

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ app.use(cors());
 // -------------------------
 
 
-const review = require('./data/review.json');
-const dishesFormula = require('./data/dishesformula.json');
-const dishes = require('./data/dishes.json');
+
 
 
 //Example part start to here........
@@ -21,13 +19,3 @@ app.listen(port, () => {
 })
 
 
-// data using is start here bro......
-app.get('/chefs', (req, res) =>{
-   res.send(chefs);
-})
-
-app.get('/chefs/:id', (req, res) =>{
-  const id = req.params.id;
-  const selectedChefs = chefs.find(c=> c.id == id)
-  res.send(selectedChefs);
-})
